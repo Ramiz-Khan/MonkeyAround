ea66cd244e26307df6edebe42176ebc9434428a4
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-11-07 19:19:45 -0600

    empty routes folder/files

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+
+//dependencies
 var express = require("express");
 var sequelize = require("sequelize");
-var fs = require("fs");
-
 var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8080;
-//var db = require("./models");
 
+//setup express app
+var PORT = process.env.PORT || 8080;
 var app = express();
 
+//require db and models to sync
+var db = require("./models");
+
+//Express handles body parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+
+//routes - to be replaced by requiring route files
 app.get("/", function (req, res) {
     console.log("landingpage");
 });
@@ -38,6 +44,8 @@ app.get("/api/user/:id", function (req, res) {
 });
 
 
+
+//sync sequelize then start express
 //db.sequelize.sync().then(function() {    
     app.listen(PORT, function () {
         console.log("🌎  You're listening on port: " + PORT);
