5d83db1bf2b818cfa6b2db43e20d56579da314b6
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-11-13 17:34:21 -0600

    heroku stuff

--- models/index.js
+++ models/index.js
@@ -5,17 +5,13 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
+var config    = require(__dirname + './config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: "127.0.0.1",
-	port: process.env.PORT || 8080,
-	dialect: 'mysql'
-  });
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
