ab907b82d14c2dd2401d028c8196ede4434ea07e
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-11-13 17:25:01 -0600

    heroku stuff

--- models/index.js
+++ models/index.js
@@ -11,7 +11,11 @@ var db        = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: "127.0.0.1",
+	port: process.env.PORT || 8080,
+	dialect: 'mysql'
+  });
 }
 
 fs
